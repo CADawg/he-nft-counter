6cc3dc0b10625d27c1345a5423d1e75f45b5df47
Snaddyvitch-Dispenser <conor.howland@outlook.com>
2021-02-04 15:57:25 +0000

    Add total field

--- index.js
+++ index.js
@@ -67,13 +67,14 @@ async function queryContract({contract,table,query={}},offset=0) {
     for (let i = 0; i < nfts.length; i++) {
         if(limiter(nfts[i])) {
             if (owners.hasOwnProperty(nfts[i].account)) {
+                owners[nfts[i].account].total += 1;
                 if (owners[nfts[i].account].hasOwnProperty(grouper(nfts[i]))) {
                     owners[nfts[i].account][grouper(nfts[i])] += 1;
                 } else {
                     owners[nfts[i].account][grouper(nfts[i])] = 1;
                 }
             } else {
-                owners[nfts[i].account] = {};
+                owners[nfts[i].account] = {"total": 1};
                 owners[nfts[i].account][grouper(nfts[i])] = 1;
             }
         }
